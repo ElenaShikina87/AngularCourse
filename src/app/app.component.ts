71654cce1fcb65c697d6d5018d51aa1fc033ea61
s.shykin <aM3a3uRjsF>
2024-08-27 21:57:38 +0300

    устранение замечаний после ревью дз

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,16 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
 import {HeaderComponent} from "./components/header/header.component";
-import { ButtonModule } from 'primeng/button';
+import {ButtonModule} from 'primeng/button';
 import {MainComponent} from "./components/main/main.component";
-import { CascadeSelectModule } from "primeng/cascadeselect";
+import {CascadeSelectModule} from "primeng/cascadeselect";
+import {ImageModule} from "primeng/image";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, ButtonModule, MainComponent, CascadeSelectModule],
+  imports: [RouterOutlet, HeaderComponent, ButtonModule, MainComponent, CascadeSelectModule, ImageModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 
